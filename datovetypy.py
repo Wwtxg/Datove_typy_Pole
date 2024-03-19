5d03ce22f5e2e32ab08bfe980ee6988f9b639def
Nikita <nikitamolavko27@gmail.com>
2024-03-19 09:29:47 +0100

    pole

---
+++ datovetypy.py
@@ -0,0 +1,12 @@
+
+arrays = ["Ford", "Porsche", "Audi", "BMW", "Mercedes", "Škoda"]
+
+
+for x in range(1):
+    print ("1 " + arrays[0])
+    print ("2 " + arrays[1])
+    print ("3 " + arrays[2])
+    print ("4 " + arrays[3])
+    print ("5 " + arrays[4])
+    print ("6 " + arrays[5])
+
