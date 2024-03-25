47da8a92979734e5aeae3afa3485c3fe1cb3dd97
Nikita <nikitamolavko27@gmail.com>
2024-03-25 09:27:48 +0100

    pop a remove a append

--- datovetypy.py
+++ datovetypy.py
@@ -2,12 +2,22 @@ cars = ["Ford", "Volvo", "BMW", "AUDI"]
 
 print (cars)
 
-for x in range(len(cars)):
-    print(f"Auto {x+1}: {cars[x]}")
+def vypis_pole(prvek):
+    for x in range(len(prvek)):
+        print(f"Auto {x+1}: {cars[x]}")
 
 carplus = input("Zadejte auto: ")
 
 cars.append(carplus)
+vypis_pole(cars)
 
-for x in range(len(cars)):
-    print(f"Auto {x+1}: {cars[x]}")
+print ("   ")
+
+carminusstr = str(input("Co odebrat str "))
+cars.remove(carminusstr)
+vypis_pole(cars)
+
+carminus = int(input("Co odebrat "))
+cars.pop(carminus)
+vypis_pole(cars)
+
